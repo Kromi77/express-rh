e7e4a9d4514a5d4c5b56f3468e15ab4c844b3501
Kromi77 <mrozugamepolska@gmail.com>
2025-03-17 01:09:40 +0100

    fix: variable name

--- src/index.ts
+++ src/index.ts
@@ -144,7 +144,7 @@ class ERH {
 		this._host = host ?? "localhost";
 		this._port = port ?? 3000;
 
-		if (this._useParentPath && this._baseRoute != "/") throw new Error(pc.red("ERH > You can't use parent path with base route"));
+		if (useParentPath && this._baseRoute != "/") throw new Error(pc.red("ERH > You can't use parent path with base route"));
 		
 		this._useParentPath = useParentPath ?? false;
 		
